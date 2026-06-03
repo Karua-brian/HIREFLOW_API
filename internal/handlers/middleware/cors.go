3701b0e36d4ebe733132d3647d341e9e9befb72b
Karua Brian <karuabrian@APPLEs-MacBook-Air.local>
2026-06-03 14:24:26 +0300

    Added CORS rules

--- internal/handlers/middleware/cors.go
+++ internal/handlers/middleware/cors.go
@@ -5,9 +5,22 @@ import "net/http"
 // CORS is a middleware that adds CORS headers to the response.
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://hire-flow-frontend-sepia.vercel.app/") // 
+
+		allowedOrigins := map[string]bool{
+			"https://hire-flow-frontend-sepia.vercel.app": true,
+    		"https://hire-flow-frontend-d3e998ykv-karuas-projects.vercel.app": true,
+			
+		}
+
+		if allowedOrigins[r.Header.Get("Origin")] {
+			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin", "https://hire-flow-frontend-sepia.vercel.app/")
+		}
+		
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")		
 
 		// Handle preflight requests
 		if r.Method == http.MethodOptions {
